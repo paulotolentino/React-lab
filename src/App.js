20bd43a1c3703df1c402beec58eceed4713efb6d
Paulo Sergio de Jesus Tolentino <paulo.tolentino@venturus.org.br>
2022-04-25 16:10:58 -0300

    Add React.useCallback example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = React.useState("");
+  const [textToChangeMemiozedFn, setTextToChangeMemiozedFn] =
+    React.useState("");
+  const fn = () => {};
+
+  const MemoizedFn = React.useCallback(fn, [textToChangeMemiozedFn]);
+
+  React.useEffect(() => {
+    console.log("fn has changed");
+  }, [fn]);
+
+  React.useEffect(() => {
+    console.log("MemoizedFn has changed");
+  }, [MemoizedFn]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      No change MemoizedFn
+      <input
+        type="text"
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+      />
+      Change MemoizedFn
+      <input
+        type="text"
+        value={textToChangeMemiozedFn}
+        onChange={(event) => setTextToChangeMemiozedFn(event.target.value)}
+      />
     </div>
   );
 }
