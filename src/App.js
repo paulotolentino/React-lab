099512545f966c1322ebc2abf65baa684e93fee1
Paulo Sergio de Jesus Tolentino <paulo.tolentino@venturus.org.br>
2022-04-25 16:37:20 -0300

    Add React.useMemo example

--- src/App.js
+++ src/App.js
@@ -3,33 +3,46 @@ import "./App.css";
 
 function App() {
   const [text, setText] = React.useState("");
-  const [textToChangeMemiozedFn, setTextToChangeMemiozedFn] =
+  const [textToChangeMemiozedValue, setTextToChangeMemiozedValue] =
     React.useState("");
-  const fn = () => {};
 
-  const MemoizedFn = React.useCallback(fn, [textToChangeMemiozedFn]);
+  const sum = () => {
+    const a = textToChangeMemiozedValue;
+    if (!a) return 0;
+    if (a === "0") return null;
+    return Number(a) / 2;
+  };
+
+  const memoizedSum = React.useMemo(() => {
+    const a = textToChangeMemiozedValue;
+    if (!a) return 0;
+    if (a === "0") return null;
+    return Number(a) / 2;
+  }, [textToChangeMemiozedValue]);
 
   React.useEffect(() => {
-    console.log("fn has changed");
-  }, [fn]);
+    console.log("sum has changed");
+  }, [sum]);
 
   React.useEffect(() => {
-    console.log("MemoizedFn has changed");
-  }, [MemoizedFn]);
+    console.log("memoizedSum has changed");
+  }, [memoizedSum]);
 
   return (
     <div className="App">
-      No change MemoizedFn
+      <p>{sum()}</p>
+      <p>{memoizedSum}</p>
+      No change memoizedSum
       <input
         type="text"
         value={text}
         onChange={(event) => setText(event.target.value)}
       />
-      Change MemoizedFn
+      Change memoizedSum
       <input
-        type="text"
-        value={textToChangeMemiozedFn}
-        onChange={(event) => setTextToChangeMemiozedFn(event.target.value)}
+        type="number"
+        value={textToChangeMemiozedValue}
+        onChange={(event) => setTextToChangeMemiozedValue(event.target.value)}
       />
     </div>
   );
